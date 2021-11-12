8685c9642a2268a23e2924cee959437018401bdb
skynocover <skynocover@Gmail.com>
2021-11-12 18:21:49 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
+    './temp/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
