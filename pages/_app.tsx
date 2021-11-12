e800a935a6e396fa6af5ea804517c108841ca0b9
skynocover <skynocover@Gmail.com>
2021-11-12 17:46:51 +0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { AppContext, AppProvider } from '../components/AppContext';
-import '../styles/globals.css';
+// import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'antd/dist/antd.css';
 // import '../mainstyle.css';
-import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   React.useEffect(() => {}, []);
